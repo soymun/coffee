0e31fe98d8e699e3d292740d992dc756f9d9fcb9
Семён Власов <semen.vlasov@aisa.ru>
2024-04-18 13:42:58 +0300

    test

--- vaadin/src/main/java/com/example/demo/elements/MyElement.java
+++ vaadin/src/main/java/com/example/demo/elements/MyElement.java
@@ -3,13 +3,9 @@ package com.example.demo.elements;
 import com.example.demo.gateways.CoffeeGateway;
 import com.vaadin.flow.component.ClientCallable;
 import com.vaadin.flow.component.Tag;
-import com.vaadin.flow.component.Text;
 import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.component.notification.Notification;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
-
-import java.util.concurrent.Callable;
-import java.util.concurrent.CompletableFuture;
 
 @Tag("my-element")
 public class MyElement extends VerticalLayout {
@@ -27,21 +23,18 @@ public class MyElement extends VerticalLayout {
     public void getCurrentStatus() {
         if (coffeeGateway != null) {
             if (comboBox.getValue() != null) {
-                getUI().get().access(() -> {
-                    if(statusPrev == null){
-                        statusPrev = coffeeGateway.getStatus(comboBox.getValue());
-                    } else {
-                        String status = coffeeGateway.getStatus(comboBox.getValue());
-                        if (!status.equals(statusPrev)) {
-                            Notification notification = new Notification();
-                            notification.add("Статус - " + status);
-                            statusPrev = status;
-                            notification.open();
-                            add(notification);
-                        }
+                if (statusPrev == null) {
+                    statusPrev = coffeeGateway.getStatus(comboBox.getValue());
+                } else {
+                    String status = coffeeGateway.getStatus(comboBox.getValue());
+                    if (!status.equals(statusPrev)) {
+                        Notification notification = new Notification();
+                        notification.add("Статус - " + status);
+                        statusPrev = status;
+                        notification.open();
+                        add(notification);
                     }
-
-                });
+                }
             }
         }
     }
